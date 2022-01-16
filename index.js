a8cc9e8845ac3e77d45d790d81d880d68a1c6f21
rakibtweets <rakibhasanroky0@gmail.com>
2022-01-17 01:40:13 +0600

    environment variable

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const app = require('./app');
+const dotenv = require('dotenv');
 const port = process.env.PORT || 5000;
 
+//setting up config file
+dotenv.config({ path: 'Config/config.env' });
+
 app.listen(port, () => {
-  console.log('Server is running at port', port);
+  console.log(
+    `Server is running at port ${port} in ${process.env.NODE_MODE} mode`
+  );
 });
