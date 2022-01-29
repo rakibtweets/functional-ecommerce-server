400790c9d6b46bcdcf47f56fe7d10523411b510a
rakibtweets <rakibhasanroky0@gmail.com>
2022-01-29 22:24:05 +0600

    generate forgot password token

--- Modals/userModels.js
+++ Modals/userModels.js
@@ -2,6 +2,7 @@ const moongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const userSchema = new moongoose.Schema({
   name: {
@@ -61,6 +62,25 @@ userSchema.methods.getJwtToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_TIME,
   });
+};
+
+// generate password reset token
+
+userSchema.methods.getResetPasswordToken = function () {
+  // generate token
+
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  // Hash it and set to resetPasswordToken
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  // Set token Expire Time
+  this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+
+  return resetToken
 };
 
 module.exports = moongoose.model('user', userSchema);
